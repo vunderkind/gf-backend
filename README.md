3a6d4c0965e62c735741b2493179bbcb78f804ae
mogwai <feelirabs@gmail.com>
2020-03-29 23:55:03 +0100

    Massive dump

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# gf-backend
-goodfaithbackend
+# good-faith
+"The purest form of giving is from anonymous to anonymous" - Jay Z
---
+++ data/migrations/20200326032443_beneficiaries.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('beneficiaries', tbl => {
+        tbl.increments();
+        tbl.text('firstName', 128).notNullable();
+        tbl.text('lastName', 128).notNullable();
+        tbl.string('phone', 128).unique().notNullable();
+        tbl.boolean('isWhatsApp').notNullable();
+        tbl.string('email').unique();
+        tbl.string('context').notNullable();
+        tbl.string('bankName').notNullable();
+        tbl.string('accountNumber').unique().notNullable();
+        tbl.string('accountName').unique().notNullable();
+        tbl.string('paymentLink').unique();
+        tbl.string('location').notNullable();
+        tbl.string('socialMedia').unique();
+        tbl.boolean('isVerified');
+        tbl.timestamp('created_at').defaultTo(knex.fn.now());
+
+        
+    })
+  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('beneficiaries');
+  
+};
