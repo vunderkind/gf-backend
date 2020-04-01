9218ca79b5301c9f25d1de2d142c5d57b850039c
Olumide Falomo <olumide@kudi.co>
2020-04-01 09:45:27 +0100

    chore: Move error handler to index file

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ server.get('/api', (req, res) => {
   })
 });
 
+// handle not found errors
+server.use((req, res, next) => {
+  const err = new Error('Route Not Found');
+  err.status = 404;
+  err.name = 'NotFoundError'
+  next(err);
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error: {
+      name: err.name,
+      message: err.message,
+    }
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`\n*** Good Faith is Listening on port ${PORT} ***\n`);
--- server.js
+++ server.js
@@ -16,21 +16,4 @@ server.use(express.json());
 // server.use(basicAuth)
 server.use('/api/', beneficiaryRouter);
 
-// handle not found errors
-server.use((req, res, next) => {
-  const err = new Error('Route Not Found');
-  err.status = 404;
-  err.name = 'NotFoundError'
-  next(err);
-});
-
-server.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    error: {
-      name: err.name,
-      message: err.message,
-    }
-  });
-});
-
 module.exports = server;
